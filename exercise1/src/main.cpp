5dc18f0fe1bfe268d1b2f4ce58491437e296064e
Markus Hinsche <markus_hinsche@hotmail.com>
2013-04-23 15:17:27 +0200

    XCode folder structure + gitignore update

---
+++ exercise1/src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exercise1
+//
+//  Created by Markus Hinsche on 4/23/13.
+//  Copyright (c) 2013 Markus Hinsche. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
