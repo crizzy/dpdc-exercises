5cb0477afc56ba8773b8edce95ffc1243f7ee07f
Markus Hinsche <markus_hinsche@hotmail.com>
2013-04-23 19:19:51 +0200

    testfile from lecture + begin of function isUniqueColumnCombination

--- exercise1/src/main.cpp
+++ exercise1/src/main.cpp
@@ -10,11 +10,14 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <list>
+#include <algorithm>    // std::unique, std::distance
+
 
 #ifdef WIN32
-#define DATA_PATH "../data/uniprot.tsv"
+#define DATA_PATH "../data/testfile_from_lecture.tsv"
 #else
-#define DATA_PATH "/Users/Markus/Development/C++/dpdc-exercises/exercise1/data/uniprot.tsv"
+#define DATA_PATH "/Users/Markus/Development/C++/dpdc-exercises/exercise1/data/testfile_from_lecture.tsv"
 #endif
 
 
@@ -24,14 +27,41 @@
 
 typedef std::vector<std::string> ColumnVector;
 
-int main(int argc, const char * argv[])
+// global vars
+std::vector<ColumnVector> columns;
+
+
+bool isUniqueColumnCombination(std::vector<int> &combination)
+{//TODO make it work
+    
+//    for (int i = 0; i < combination.size(); i++)
+//    {
+//        columns[combination.at(i)];
+//    }
+    
+    size_t sizeBefore = columns[combination.at(0)].size();
+    std::vector<std::string> column = columns[combination.at(0)];
+    
+    ColumnVector::iterator it = std::unique(columns[combination.at(0)].begin(), //TODO: do this for multiple columns
+                                         columns[combination.at(0)].end());
+    size_t sizeAfter = it - columns[combination.at(0)].begin();
+    
+    if (sizeAfter == sizeBefore)
+        return true;
+    
+    return false;
+}
+
+
+
+
+int readColumnsFromFile()
 {
-	std::vector<ColumnVector> columns;
 	for (int i = 0; i<223; i++)
 	{
 		columns.push_back(ColumnVector());
 	}
-
+    
 	// read column names:
 	std::ifstream I(DATA_PATH);
 	if (!I.is_open())
@@ -39,35 +69,58 @@ int main(int argc, const char * argv[])
 		std::cout << "Cannot find file \"uniprot.tsv\"" << std::endl;
 		return 1;
 	}
-
+    
 	char rowBuffer[BUFFER_SIZE];
 	char cellBuffer[BUFFER_SIZE];
-
+    
 	for (int rowIndex = 0; !I.eof(); rowIndex++)
 	{
 		// read a whole line into the buffer:
 		I.getline(rowBuffer, BUFFER_SIZE, '\n');
 		if (strlen(rowBuffer) == 0)
 			break;
-
+        
 		std::stringstream s_rowBuffer(rowBuffer);
-
+        
 		for (int colIndex = 0; !s_rowBuffer.eof(); colIndex++)
 		{
 			s_rowBuffer.getline(cellBuffer, BUFFER_SIZE, '\t');
 			columns.at(colIndex).push_back(cellBuffer);
-			//std::cout << cellBuffer << "\t";
+			std::cout << cellBuffer << "\t";
 		}
-		//std::cout << std::endl;
-		if (rowIndex % 54000 == 0)
-			std::cout << "read " << rowIndex << " rows." << std::endl;
-
+		std::cout << std::endl;
+//		if (rowIndex % 54000 == 0)
+//			std::cout << "read " << rowIndex << " rows." << std::endl;
 	}
-
-	char c;
-	std::cout << "Finished!" << std::endl;
-	std::cin >> c;
-
+    
+		
+    
     return 0;
+}
+
+int main(int argc, const char * argv[])
+{
+    // read stuff
+    readColumnsFromFile();
+    
+    // check if read correctly
+//    for(ColumnVector::iterator it = columns[0].begin(); it != columns[0].end();it++)
+//    {
+//        std::cout << *it<<std::endl;
+//    }
+    
+    
+    // unique column combinations
+    std::vector<int> combination = {3};
+    
+    isUniqueColumnCombination(combination) ?
+    std::cout <<"true" << std::endl:
+    std::cout <<"false" << std::endl;
+    
+    
+    
+    // end
+    std::cout << "Finished!" << std::endl;
+    char c; std::cin >> c;
 }
 
