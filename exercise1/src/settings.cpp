f961e4ec4282b1b6fc58d7d35db0e043756b3269
Markus Hinsche <markus_hinsche@hotmail.com>
2013-04-29 17:03:02 +0200

    settings minor change

--- exercise1/src/settings.cpp
+++ exercise1/src/settings.cpp
@@ -18,6 +18,8 @@
 #include <time.h>
 #include <iomanip>
 
+#define CHECK_RESULT // change DATA_PATH to uniport.tsv if enabled
+
 #ifdef _WIN32
 #define DATA_PATH "../data/uniprot.tsv"
 #else
@@ -26,7 +28,7 @@
 #endif
 
 #ifdef _WIN32
-#define RESULT_PATH "../results/uniprot_20000rows.tsv_uniques_density0.01.txt"
+#define RESULT_PATH "../results/uniprot_10000rows.tsv_uniquesSensitivity0.02.txt"
 //uniprot_20000rows.tsv_uniques_density0.01.txt
 //uniprot_20000rows.tsv_uniques_14, 103 with 176_density0.txt
 //uniprot_20000rows.tsv_uniques_up to 8-14 with 175 at density 0.0.txt
@@ -34,9 +36,8 @@
 #define RESULT_PATH "/Users/Markus/Development/C++/dpdc-exercises/exercise1/results/uniprot_700rows.tsv_uniques_up_to_4er_combinations_sensity0.0.txt"
 #endif
 
-#define CHECK_RESULT
 
 
 //#define VERBOSE
-const float searchDensity = 0.0; // the bigger the more we throw out
+const float searchDensity = 0.02; // the bigger the more we throw out
 
